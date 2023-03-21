03248382fe2bc3c9e8288b8d63dbc23ba3cb299d
HossameTor <hossametorchi07@gmail.com>
2023-03-21 16:12:51 +0000

    add de

---
+++ my-de/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import JeuDe from './Composants/JeuDe'
+function App() {
+  return (
+    <div className="App">
+      <JeuDe cache={5} />
+    </div>
+  );
+}
+
+export default App;
